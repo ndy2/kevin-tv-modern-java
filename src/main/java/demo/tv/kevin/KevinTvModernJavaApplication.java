7ca19aeb04d1b31ceccee71ec748f82e28714ba4
ndy2 <emrdbs12@gmail.com>
2022-02-26 09:40:20 +0900

    initial commit

---
+++ src/main/java/demo/tv/kevin/KevinTvModernJavaApplication.java
@@ -0,0 +1,13 @@
+package demo.tv.kevin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KevinTvModernJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KevinTvModernJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/demo/tv/kevin/KevinTvMordernJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.tv.kevin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KevinTvMordernJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
